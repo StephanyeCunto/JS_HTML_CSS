d0ef408c29b6adf64478a9a7b245d6cae17f95be
Stephanye Cunto <89281357+StephanyeCunto@users.noreply.github.com>
2024-04-29 18:13:19 -0300

    Add files via upload

---
+++ seção 3/aula 11/index.js
@@ -0,0 +1,32 @@
+let nome = 'Ste' // criando
+var nome2 = 'Ste'
+
+//let nome='Cris'
+//Let tem es opo de bloco {bloco}
+//Var só tem escopo de função
+var nome2='Cris'
+
+
+const verdadeira =true
+if(verdadeira){
+    let nome='Cris' //criando
+    console.log(nome,nome2)
+}
+
+if(verdadeira){
+    let nome = 'Outra coisa'
+    var nome2='Stephanye'
+}
+
+console.log(nome, nome2)
+
+var sobrenome= 'Miranda'
+function falaOi(){
+    var nome3='Luiz'
+    console.log(sobrenome)
+}
+
+//console.log(nome3)
+
+falaOi();
+
---
+++ seção 3/aula 12/index.js
@@ -0,0 +1,19 @@
+// ... rest, ...spread
+const numeros=[[1,2,3],[4,5,6],[7,8,9]]
+const [,[,,seis]]=numeros
+const [lista1, lista2, lista3]=numeros
+
+console.log(lista3)
+
+
+/*
+const numeros= [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000]
+const [um, , três, ,cinco, , sete]=numeros
+console.log(um,três,cinco)
+*/
+/*
+const [primeiroNumero,segundoNumero,terceiroNumero,...resto]=numeros
+
+console.log(1,2,3)
+console.log(resto)
+*/
